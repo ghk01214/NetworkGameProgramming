a53b32ae1a494e8a5e0a4b236a712f2bbf3c798c
ghk01214 <ghk01214@naver.com>
2021-10-10 13:07:01 +0900

    pch 제거

--- WindowSocketProgramming/WindowSocketProgramming/Server(telnet).cpp
+++ WindowSocketProgramming/WindowSocketProgramming/Server(telnet).cpp
@@ -1,4 +1,15 @@
-#include "pch.h"
+#pragma comment(lib, "ws2_32")
+#pragma warning(disable : 4996)
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#define SERVERPORT 9000
+#define BUFFERSIZE 512
 
 void ErrorQuit(std::string msg);
 void DisplayError(std::string msg);
