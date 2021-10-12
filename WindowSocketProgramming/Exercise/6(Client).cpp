b9099ec955dc79a6800477e4d0822b3a8c5c91b0
ghk01214 <ghk01214@naver.com>
2021-10-12 16:02:54 +0900

    파일 전송 TCP 서버(고정+가변)

--- WindowSocketProgramming/Exercise/6(Client).cpp
+++ WindowSocketProgramming/Exercise/6(Client).cpp
@@ -20,20 +20,15 @@ std::default_random_engine dre(rd());
 void ErrorQuit(std::string msg);
 void DisplayError(std::string msg);
 SOCKET InitSocket();
-size_t ReadFile(std::vector<std::string>* v);
-void SendFile(SOCKET connectSocket, size_t fileSize, std::vector<std::string>* vData);
+int ReadFile(SOCKET connectSocket);
+int SendFile(SOCKET connectSocket, std::string* sBytes, int nLength, int* nReturnVal);
 
 int main()
 {
 	SOCKET connectSocket{ InitSocket() };
 
-	std::vector<std::string> vData;
-	size_t					 fileSize{ ReadFile(&vData) };
-
-	if (fileSize == -1)
+	if (!ReadFile(connectSocket))
 		return 0;
-
-	SendFile(connectSocket, fileSize, &vData);
 
 	closesocket(connectSocket);
 	WSACleanup();
@@ -92,9 +87,10 @@ SOCKET InitSocket()
 }
 
 // 전송할 파일의 내용을 읽어들이는 함수
-size_t ReadFile(std::vector<std::string>* vData)
+int ReadFile(SOCKET connectSocket)
 {
 	std::string sFileName;
+	std::vector<std::string> vData;
 
 	std::cout << "전송하고자 하는 파일의 이름 : ";
 	std::getline(std::cin, sFileName);
@@ -104,11 +100,13 @@ size_t ReadFile(std::vector<std::string>* vData)
 	if (fSendFile.fail())
 	{
 		std::cout << "파일 읽기 실패" << std::endl;
-		return -1;
+		return 0;
 	}
 	
-	std::string				 sData;
-	size_t					 fileSize;
+	std::string	 sData;
+	size_t		 fileSize;
+	int			 nLength{};
+	int			 nReturnVal;
 
 	// 파일의 크기 확인
 	fSendFile.seekg(0, std::ios::end);
@@ -117,17 +115,24 @@ size_t ReadFile(std::vector<std::string>* vData)
 
 	// 전송할 파일의 사이즈를 먼저 vector에 push한 다음 파일 이름을 push한다
 	// 전송 순서 : 파일 크기 -> 파일 이름 -> 파일 데이터
-	vData->push_back(std::to_string(fileSize));
-	vData->push_back(sFileName);
+	vData.push_back(sFileName);
+	vData.push_back(std::to_string(fileSize));
+
+	for (std::string sBytes : vData)
+	{
+		if (!SendFile(connectSocket, &sBytes, nLength, &nReturnVal))
+			break;
+	}
 
 	// 전송하는 데이터를 가변 길이로 하기 위해 매번 전송하는 바이트 수를 50~ 512 바이트 사이에서 랜덤으로 설정
 	std::uniform_int_distribution<>	 uid(MINBUFSIZE, MAXBUFSIZE);
 	size_t							 uploadSize{};
+	int								 randSize;
 
-	while (true)
+	do
 	{
 		// 가변 데이터 길이 랜덤 설정
-		int randSize{ uid(dre) };
+		randSize = uid(dre);
 		uploadSize += randSize;
 
 		// 전송한 총 바이트 수가 파일의 크기보다 클 경우 파일 사이즈에 맞춰서 전송 바이트 용량을 맞춘다
@@ -136,56 +141,44 @@ size_t ReadFile(std::vector<std::string>* vData)
 			uploadSize -= randSize;
 			randSize = fileSize - uploadSize;
 			uploadSize += randSize;
-
-			sData.resize(randSize);
-			fSendFile.read(sData.data(), sData.size());
-			vData->push_back(sData);
-
-			break;
 		}
 
 		sData.resize(randSize);
 		fSendFile.read(sData.data(), sData.size());
-		vData->push_back(sData);
-	}
 
-	return fileSize;
-}
-
-// 파일 전송
-void SendFile(SOCKET connectSocket, size_t fileSize, std::vector<std::string>* vData)
-{
-	int		 nLength;
-	int		 nReturnVal;
-	size_t	 uploadSize{};
-
-	for (std::string sBytes : *vData)
-	{
-		nLength = sBytes.length();
-		nReturnVal = send(connectSocket, std::to_string(nLength).data(), sizeof(int), 0);
-
-		if (nReturnVal == SOCKET_ERROR)
-		{
-			DisplayError("send(길이)");
+		if (!SendFile(connectSocket, &sData, nLength, &nReturnVal))
 			break;
-		}
-
-		nReturnVal = send(connectSocket, sBytes.data(), nLength, 0);
-
-		if (nReturnVal == SOCKET_ERROR)
-		{
-			DisplayError("send(데이터)");
-			break;
-		}
-
-		uploadSize += nLength;
 
 		std::cout.precision(4);
 		std::cout << "[TCP 클라이언트] " << sizeof(int) << " + " << nReturnVal << "바이트를 보냈습니다." << std::endl;
 		std::cout << "업로드 중(" << static_cast<float>(uploadSize) / fileSize * 100 << "%)   " << std::endl;
 		std::cout << "\x1B[2A";
-	}
+	} while (fileSize != uploadSize);
 
 	std::cout << "\x1B[B\x1B[2K\x1B[A\x1B[K";
 	std::cout << "업로드 완료(" << static_cast<float>(uploadSize) / fileSize * 100 << "%, 총 " << fileSize << "바이트)" << std::endl;
+
+	return 1;
+}
+
+int SendFile(SOCKET connectSocket, std::string* sData, int nLength, int* nReturnVal)
+{
+	nLength = sData->length();
+	*nReturnVal = send(connectSocket, std::to_string(nLength).data(), sizeof(int), 0);
+
+	if (*nReturnVal == SOCKET_ERROR)
+	{
+		DisplayError("send(길이)");
+		return 0;
+	}
+
+	*nReturnVal = send(connectSocket, sData->data(), nLength, 0);
+
+	if (*nReturnVal == SOCKET_ERROR)
+	{
+		DisplayError("send(데이터)");
+		return 0;
+	}
+
+	return 1;
 }
